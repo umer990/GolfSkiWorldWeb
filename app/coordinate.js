5ab7f52121b9838d07cc59552544c2a2ae376350
umer990 <umerkhan@outlook.com>
2016-10-07 12:09:02 +0200

    coordinates.ts move to coordinate.ts

---
+++ app/coordinate.js
@@ -0,0 +1,18 @@
+"use strict";
+var Coordinates = (function () {
+    function Coordinates(lat, lng, zoomMin, zoomMax, count, country, city, type, infoWindow, iconUrl) {
+        this.lat = lat;
+        this.lng = lng;
+        this.zoomMin = zoomMin;
+        this.zoomMax = zoomMax;
+        this.count = count;
+        this.country = country;
+        this.city = city;
+        this.type = type;
+        this.infoWindow = infoWindow;
+        this.iconUrl = iconUrl;
+    }
+    return Coordinates;
+}());
+exports.Coordinates = Coordinates;
+//# sourceMappingURL=coordinate.js.map
